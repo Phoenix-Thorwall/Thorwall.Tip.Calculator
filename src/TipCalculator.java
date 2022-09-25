572c85f76368deca79639aab740198780e2f8282
Phoenix <phoenix.thorwall@gmail.com>
2022-09-25 14:03:36 -0400

    I Finished the Tip Calculator Project!!!!

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -9,11 +9,17 @@ public class TipCalculator
         Scanner question = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
+        //Initializing
         double price = 0;
         double totalB4Tip = 0;
         double totalTip = 0;
+        double indCostB4Tip = 0;
+        double indTip = 0;
+        double totalBillWithTip = 0;
+        double totalCostPerPerson = 0;
 
 
+        //Lore: Introducing the User to the Program
         System.out.println("");
         System.out.println("Welcome to the tip calculator!");
         System.out.println("");
@@ -22,7 +28,9 @@ public class TipCalculator
 
         System.out.print("What is the tip percentage? (0 - 100): ");
         int tip = question.nextInt();
+        System.out.println("");
 
+        //Core of the Program
         while (price != -1)
         {
             System.out.print("Enter the cost of an item in dollars and cents (Type -1 to End): ");
@@ -34,17 +42,34 @@ public class TipCalculator
 
         System.out.println("--------------------------------------------------------");
 
+        //Formatting My Variables for the Final Product
 
-        totalTip = (tip / 100) * totalB4Tip;
+        totalTip =  ((double) tip) / 100 * totalB4Tip;
+        String formattedTotalTip = formatter.format(totalTip);
+
+        indCostB4Tip = totalB4Tip / partySize;
+        String formattedIndCostB4Tip = formatter.format(indCostB4Tip);
+
+        indTip = totalTip / partySize;
+        String formattedIndTip = formatter.format(indTip);
+
+        totalBillWithTip = totalB4Tip + totalTip;
+        String formattedTotalBWT = formatter.format(totalBillWithTip);
+
+        totalCostPerPerson = indCostB4Tip + indTip;
+        String formattedTotalCPP = formatter.format(totalCostPerPerson);
 
 
+
+
+        //Final Product
         System.out.println("Total Bill Before Tip: " + totalB4Tip);
-        System.out.println("Tip Percentage: ");
-        System.out.println("Total Tip: ");
-        System.out.println("Total Bill With Tip: ");
-        System.out.println("Per Person Cost Before Tip: ");
-        System.out.println("Tip Per Person: ");
-        System.out.println("Total Cost Per Person: ");
+        System.out.println("Tip Percentage: " + tip);
+        System.out.println("Total Tip: " + formattedTotalTip);
+        System.out.println("Total Bill With Tip: " + formattedTotalBWT);
+        System.out.println("Per Person Cost Before Tip: " + formattedIndCostB4Tip);
+        System.out.println("Tip Per Person: " + formattedIndTip);
+        System.out.println("Total Cost Per Person: " + formattedTotalCPP);
 
 
 
